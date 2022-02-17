911417977551797704156f597d10e189193306ee
Casey Harding <casey.r.harding@gmail.com>
2022-02-17 13:20:54 -0500

    Through Friend component.

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-/**
-.friend {
-  width: 60%;
-  display: flex;
-  justify-content: space-between;
-}
-*/
+const kf = keyframes`
+  50% {
+    transform: scale(0.8);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`
+
 const StyledFriend = styled.div`
   width: 60%;
   display: flex;
@@ -27,11 +30,26 @@ const StyledFriend = styled.div`
     transition: all 0.2s ease-in-out;
     background-color: ${pr => pr.theme.secondaryColor};
   }
+
+  &::before {
+    content: "${pr => pr.besty ? '💚' : '😀'}";
+  }
+
+  button {
+    background-color: ${pr => pr.theme.tertiaryColor};
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+
+  transform: scale(2); // start of the animation
+  opacity: 0; // start of the animation
+  animation: ${kf} 0.3s ease-in-out forwards;
 `
 
-export default function Friend({ info, action }) {
+export default function Friend({ info, action, besty }) {
   return (
-    <StyledFriend className='friend'>
+    <StyledFriend className='friend' besty={besty}>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
