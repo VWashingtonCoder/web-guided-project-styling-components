7b8b7970be0464f43a144905791a7464704355ca
Casey Harding <casey.r.harding@gmail.com>
2022-02-17 13:00:53 -0500

    Through to hover effects.

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+.friend {
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+}
+*/
+const StyledFriend = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  border-bottom: 2px solid white;
+
+  background-color: ${pr => pr.theme.primaryColor};
+  color: ${pr => pr.theme.white};
+
+  @media ${pr => pr.theme.breakpointMobile} {
+    width: initial;
+  }
+
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: ${pr => pr.theme.secondaryColor};
+  }
+`
 
 export default function Friend({ info, action }) {
   return (
-    <div className='friend'>
+    <StyledFriend className='friend'>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
