cac4b47b55be027c0805169fa583bfa1e7b28c02
Vincent Washington <97264834+VWashingtonCoder@users.noreply.github.com>
2022-03-03 09:41:47 -0700

    Final Solution_index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ worker.start()
 
 import React from 'react'
 import { render } from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/App'
 
 import './styles.css'
 
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+
 render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  ,
   document.querySelector('#root')
 )
